f97b923b51e5bf0400e151608e9da8497ebb0a17
TimothyPav <1000timka@gmail.com>
2024-06-13 21:23:12 -0500

    finialized schemas and added all databases and their layouts

--- backend/src/Database_Operations.cpp
+++ backend/src/Database_Operations.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     sqlite3* DB; 
     int exit = 0; 
 
-    exit = sqlite3_open("../databases/players.db", &DB); 
+    exit = sqlite3_open("../databases/tracer.db", &DB); 
   
     if (exit) { 
         std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl; 
@@ -26,15 +26,19 @@ int main(int argc, char** argv)
         std::cout << "Opened Database Successfully!" << std::endl; 
 
     const char* sqlCreateTable = R"(
-    CREATE TABLE "players" (
-    "ID"	INTEGER NOT NULL UNIQUE,
-    "Name"	TEXT NOT NULL,
-    "BattleTag"	TEXT NOT NULL,
-    "Tank"	TEXT NOT NULL,
-    "Damage"	TEXT NOT NULL,
-    "Support"	TEXT NOT NULL,
-    PRIMARY KEY("ID" AUTOINCREMENT)
-    );)";
+    CREATE TABLE "tracer" (
+	"Entry"	INTEGER NOT NULL UNIQUE,
+	"Blizzard_ID"	TEXT NOT NULL,
+	"Name"	TEXT NOT NULL,
+	"Deaths per 10 mins"	REAL NOT NULL,
+	"Final Blows per 10 mins"	REAL NOT NULL,
+	"Solo Kills per 10 mins"	REAL NOT NULL,
+	"Hero Damage Done per 10 mins"	REAL NOT NULL,
+	"Critical Hits per 10 mins"	REAL,
+	FOREIGN KEY("Blizzard_ID") REFERENCES "players"("Blizzard_ID"),
+	FOREIGN KEY("Name") REFERENCES "players"("Name"),
+	PRIMARY KEY("Entry" AUTOINCREMENT)
+);)";
 
     char* errMsg;
     exit = sqlite3_exec(DB, sqlCreateTable, 0, 0, &errMsg);
